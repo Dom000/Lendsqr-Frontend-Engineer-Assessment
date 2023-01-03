c7dc203ab460e7a47baaa9fdbd3cc3279d04ce76
dom000 <godknowsegiboy00@gmail.com>
2023-01-03 18:52:31 +0100

    paginations added to tables

--- src/pages/user/Users.tsx
+++ src/pages/user/Users.tsx
@@ -1,11 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../../components/cards/Card";
-import { FiUsers } from "react-icons/fi";
-import { HiOutlineUserGroup } from "react-icons/hi";
-import { GrDocumentText } from "react-icons/gr";
+import { FiUsers, FiDatabase } from "react-icons/fi";
+import { HiOutlineUserGroup, HiOutlineDocumentText } from "react-icons/hi";
 import "./user.css";
+import { BsFilter, BsThreeDotsVertical } from "react-icons/bs";
+import { getAllusers } from "../../services/api";
+import moment from "moment";
+import { TablePagination } from "@mui/material";
 
 function Users() {
+  const [users, setUsers] = useState([]);
+
+  const loadData = async () => {
+    const userData = await getAllusers();
+    setUsers(userData);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const rows = users
+    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    .map((element: any) => (
+      <tr
+        //   onClick={() => {
+        //     router.push(`/admin/incentive_mgt/details`);
+        //   }}
+        className=" text-center trdiv1 hover:translate-x-0.5  transition-all cursor-pointer  hover:shadow-md  text-sm  text-textColor border-b"
+        key={element?.id}
+      >
+        <td className="md:text-base text-xs p-3">
+          <p>{element.orgName.substring(0, 12)}...</p>
+        </td>
+        <td className="md:text-base text-xs p-3">
+          <p>{element.userName}</p>
+        </td>
+
+        <td className="md:text-base text-xs p-3">
+          <p>{element.email.substring(0, 12)}...</p>
+        </td>
+        <td className="md:text-base text-xs p-3">
+          <p>{element.phoneNumber.substring(0, 12)}...</p>
+        </td>
+        <td className="md:text-base text-xs p-3">
+          <p>{new Date(element?.createdAt).toLocaleString()}</p>
+        </td>
+        <td className="md:text-base text-xs p-3">
+          <p>{element.orgName}</p>
+        </td>
+        <td className="md:text-base text-xs p-3">
+          <BsThreeDotsVertical />
+        </td>
+      </tr>
+    ));
+
   return (
     <div>
       <p
@@ -14,7 +76,7 @@ function Users() {
           fontSize: 18,
           letterSpacing: 3,
           color: "#213F7D",
-          marginBottom: 40,
+          marginBottom: 30,
         }}
       >
         Users
@@ -33,16 +95,77 @@ function Users() {
           Subtitle={2000}
         />
         <Card
-          icon={<GrDocumentText style={{ color: "#DF18FF" }} />}
-          iconBg={"#e018ff28"}
+          icon={<HiOutlineDocumentText style={{ color: "#F55F44" }} />}
+          iconBg={"#FEEFEC"}
           title="USERS WITH LOANS"
           Subtitle={2000}
         />
         <Card
-          icon={<FiUsers style={{ color: "#DF18FF" }} />}
-          iconBg={"#e018ff28"}
+          icon={<FiDatabase style={{ color: "#FF3366" }} />}
+          iconBg={"#FFEBF0"}
           title="USERS ITH SAVINGS"
           Subtitle={2000}
+        />
+      </div>
+
+      {/* table ################################# */}
+
+      <div id="tableCard">
+        <table>
+          <thead>
+            <tr>
+              <td className="">
+                <div className=" tddiv ">
+                  <p>organization</p>
+                  <BsFilter />
+                </div>
+              </td>
+              <td className="">
+                <div className=" tddiv ">
+                  <p className="">Username</p>
+                  <BsFilter className="filterIcon" />
+                </div>
+              </td>
+
+              <td className="">
+                <div className=" tddiv  ">
+                  <p>Email</p>
+                  <BsFilter className="filterIcon" />
+                </div>
+              </td>
+              <td className="">
+                <div className="tddiv ">
+                  <p className="">Phone number</p>
+                  <BsFilter className="filterIcon" />
+                </div>
+              </td>
+              <td className="">
+                <div className="tddiv  ">
+                  <p className="">Date joined</p>
+                  <BsFilter className="filterIcon" />
+                </div>
+              </td>
+
+              <td className="">
+                <div className=" tddiv ">
+                  <p className="">Status</p>
+                  <BsFilter className="filterIcon" />
+                </div>
+              </td>
+              <td className=""></td>
+            </tr>
+          </thead>
+
+          <tbody className="mx-4">{rows}</tbody>
+        </table>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 50, 100]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </div>
     </div>
---
+++ src/services/api.ts
@@ -0,0 +1,24 @@
+import { ErrorInfo } from "react";
+
+const USERAPI = "https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users"
+
+export const getAllusers = async () => {
+    try {
+        const res = await fetch(USERAPI)
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        console.log(error.message);
+    }
+}
+
+
+export const getSingleUser = async (id:string) => {
+    try {
+        const res = await fetch(USERAPI + `/${id}`)
+        const data = await res.json();
+        return data;
+    } catch (error: any) {
+        console.log(error.message);
+    }
+}
